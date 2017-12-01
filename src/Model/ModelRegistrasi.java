0a166e83608c013c742b0e23ac35bab3ad9b6cc2
MSW98 <MSW98@DESKTOP-4BCNEIF>
2017-12-01 18:42:56 +0700

    IF11_10116488_ImplementasiClassDiagram

---
+++ src/Model/ModelRegistrasi.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author MSW98
+ */
+public class ModelRegistrasi {
+    private String noKtp, nama, jKelamin="", cabang, 
+                   handuk="", paket="", biaya="";
+
+    public String getNoKtp() {
+        return noKtp;
+    }
+
+    public void setNoKtp(String noKtp) {
+        this.noKtp = noKtp;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getjKelamin() {
+        return jKelamin;
+    }
+
+    public void setjKelamin(String jKelamin) {
+        this.jKelamin = jKelamin;
+    }
+
+    public String getCabang() {
+        return cabang;
+    }
+
+    public void setCabang(String cabang) {
+        this.cabang = cabang;
+    }
+
+    public String getHanduk() {
+        return handuk;
+    }
+
+    public void setHanduk(String handuk) {
+        this.handuk = handuk;
+    }
+
+    public String getPaket() {
+        return paket;
+    }
+
+    public void setPaket(String paket) {
+        this.paket = paket;
+    }
+
+    public String getBiaya() {
+        return biaya;
+    }
+
+    public void setBiaya(String biaya) {
+        this.biaya = biaya;
+    }
+    
+    public void hargaPaket(){
+        
+    }
+    
+    public void jenisHanduk(){ 
+    }
+   
+    
+}
